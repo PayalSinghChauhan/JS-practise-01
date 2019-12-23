1a56f36d1e4ed169e92ea0660d114e76c66f2bcc
Nanu5432 <chauhannaina30@gmail.com>
2019-12-23 23:03:24 +0545

    Box Model

---
+++ rect.js
@@ -0,0 +1,14 @@
+var red = document.createElement("div");
+red.style.width="100px";
+red.style.height="100px";
+red.style.background="red";
+red.style.position="absolute";
+document.body.appendChild(red);
+
+var yellow= document.createElement("div");
+yellow.style.width="100px";
+yellow.style.height="100px";
+yellow.style.background="yellow";
+yellow.style.left="110px";
+yellow.style.position="absolute";
+document.body.appendChild(yellow);
